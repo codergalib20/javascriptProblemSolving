04d2fed8e18ba547f500ce5db161ca8282c15b19
codergalib20 <coderboygalif@gmail.com>
2021-12-16 16:28:30 +0600

    commited array duplicte array elements

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# javascriptProblemSolving
+### javascriptProblemSolving
+
+language : Javascript
+
+// remove duplicate elements from an array
+const array = e => [...new Set(e)];
+console.log(array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
---
+++ index.js
@@ -0,0 +1,10 @@
+// remove duplicate elements from an array
+const array = e =>  [...new Set(e)];
+
+console.log(array([1, 2, 3, 4, '5', 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+
+// Language: javascript
+// Path: index.js
+// remove duplicate elements from an array
+const array = e =>  [...new Set(e)];
